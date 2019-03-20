0b17947e3c5f0f4b211a23fd2cda1e6646320ac4
Samiaalbess94 <samia.albess71@gmail.com>
2019-03-20 09:53:33 +0200

    solves

--- strings.js
+++ strings.js
@@ -3,5 +3,21 @@
 // repeatStr("hello",4) ==> 'hellohellohellohello'
 	
 	function repeatStr (n, s) {
+
+		var instance={};
+		instance.repeat=repeat;
+
+
+
+		return instance;
+
+
 		//your code is here
+	}
+	var repeat=function(n,s){
+		var str=""
+		for(var i=0;i<n;i++){
+			str+=s;
+		}
+		return str;
 	}
